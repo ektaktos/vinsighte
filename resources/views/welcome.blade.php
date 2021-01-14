e21d36750080609d8a7d07b1e0c9a58425499e09
Wale Timothy <awtim01@gmail.com>
2021-01-14 21:40:05 +0100

    fixed auth bug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <prediction-component :user="{{ Auth::user() }}"></prediction-component>    
+    <prediction-component v-if="{{ Auth::user() }}" :user="{{ Auth::user() }}"></prediction-component>
+    <prediction-component v-else :user="{{ Auth::user() }}"></prediction-component>    
 @endsection
