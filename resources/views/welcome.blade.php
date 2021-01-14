827d1845365cff0caac72694478c78b4c1bf8dd8
Wale Timothy <awtim01@gmail.com>
2021-01-14 21:44:09 +0100

    fixed auth issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <prediction-component v-if="{{ Auth::user() }}" :user="{{ Auth::user() }}"></prediction-component>
-    <prediction-component v-else :user="{{ Auth::user() }}"></prediction-component>    
+    <prediction-component></prediction-component>
+    {{-- <prediction-component v-else :user="{{ Auth::user() }}"></prediction-component>     --}}
 @endsection
